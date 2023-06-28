20309317633c6ed1f669f9591a8bfc7c28f6aae7
umutesililiane <umutesililiane4@gmail.com>
2023-06-28 21:48:47 +0530

    passing data into the templates

--- base/views.py
+++ base/views.py
@@ -3,9 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name':'Lets learn python!'},
+    {'id': 2, 'name':'Design with me'},
+    {'id': 3, 'name':'Frontend developers'},
+]
+
 def home(request):
     # return HttpResponse(' Home page')
-    return render(request, 'home.html')
+    context = {'rooms':rooms}
+    return render(request, 'base/home.html',context)
 
 def room(request):
-       return render(request, 'room.html')
+       return render(request, 'base/room.html')

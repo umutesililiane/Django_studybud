750f4a25db1437feda9645f9765cc07ea56f1d09
umutesililiane <umutesililiane4@gmail.com>
2023-07-08 22:49:25 +0530

    CRUD OPERATIONS (getting form data from browser to backend)

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 
 # Create your views here.
 
@@ -19,3 +20,10 @@ def room(request, pk):
        room = Room.objects.get(id=pk)
        context = {'room': room}           
        return render(request, 'base/room.html',context)
+
+def createRoom(request):
+        form = RoomForm()
+        if request.method == 'POST':
+              print(request.POST)
+        context = {'form': form}           
+        return render(request, 'base/room_form.html',context)

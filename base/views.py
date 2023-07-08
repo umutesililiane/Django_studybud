3f5cfd69ad072e51a6494f71cc7d072855c9eb88
umutesililiane <umutesililiane4@gmail.com>
2023-07-09 00:16:00 +0530

    delete a room [Delete functionality]

--- base/views.py
+++ base/views.py
@@ -45,3 +45,9 @@ def updateRoom(request, pk):
 
        return render (request, 'base/room_form.html', context)
     
+def deleteRoom(request,pk):
+      room = Room.objects.get(id=pk)
+      if request.method == 'POST':
+            room.delete()
+            return redirect('home')
+      return render(request, 'base/delete.html',{'obj':room})
